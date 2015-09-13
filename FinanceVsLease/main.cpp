05a6c3bb1e9990d96460c1e4101b92072fa4144f
pyadmell <pyadmell@users.noreply.github.com>
2015-09-12 20:27:07 -0700

    Created by Coding Ground Client

---
+++ FinanceVsLease/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+float Get_PresentValue(float fFutureValue,float fDiscountRate)
+{
+    
+}
+
+float cash_NetPresentValue(float fCurrentCost,float fDiscountRate)
+{
+    
+}
+
+int main()
+{
+   cout << "Hello World" << endl; 
+   
+   return 0;
+}
+
